f3239fd335ca1ce0a2d806aaf96ec5699d722024
yahyaayed <146674107+yahyaayed@users.noreply.github.com>
2023-10-29 22:12:07 +0100

    Author.php

--- symfony-main/src/Entity/Author.php
+++ symfony-main/src/Entity/Author.php
@@ -82,7 +82,7 @@ class Author
     public function removeBook(Book $book): static
     {
         if ($this->books->removeElement($book)) {
-            // set the owning side to null (unless already changed)
+         
             if ($book->getAuthor() === $this) {
                 $book->setAuthor(null);
             }
